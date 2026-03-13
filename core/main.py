6aceafed2c2a3f256e3cafdaf2529e98c9459ee9
vivek-blenderlab <blenderlab24@gmail.com>
2026-03-13 06:33:07 +0530

    basic file structure done

---
+++ core/main.py
@@ -0,0 +1,17 @@
+# every feature will be treated as class 
+    # for that particular feature provided study source will be treated as obj
+    # ex pdf provided then obj-pdf of class pdf will be created and then all further operation will be based on class defination 
+
+
+# some of the functions like embedding , storing data into chroma , retriver will be witten into base class and all other features will inherit the base
+# system prompts will be taken for sys_propots which is later editable
+# 
+
+
+
+g
+
+
+
+
+
